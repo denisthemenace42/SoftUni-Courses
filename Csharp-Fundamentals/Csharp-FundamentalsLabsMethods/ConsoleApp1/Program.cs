27f9ae0546d5ed1a8725a452d4f5720a75c92d5b
Denis Mehmed <133818148+denisthemenace42@users.noreply.github.com>
2023-06-02 02:28:55 +0300

    Learning methods

---
+++ Csharp-Fundamentals/Csharp-FundamentalsLabsMethods/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void signOfIntegerNumber(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            signOfIntegerNumber(inputNumber);
+
+        }
+    }
+}
---
+++ Csharp-Fundamentals/Csharp-FundamentalsLabsMethods/P02.Grades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P02.Grades
+{
+    internal class Program
+    {
+        static void printGrade(double number)
+        {
+            if (number <= 2.99)
+            {
+                Console.WriteLine($"Fail");
+            }
+            else if (number >= 3.00 && number <= 3.49)
+            {
+                Console.WriteLine($"Poor");
+            }
+            else if (number>= 3.50 && number<=4.49)
+            {
+                Console.WriteLine($"Good");
+            }
+            else if (number >= 4.50 && number <= 5.49)
+            {
+                Console.WriteLine($"Very good");
+            }
+            else
+            {
+                Console.WriteLine($"Excellent");
+            }
+        }
+        static void Main(string[] args)
+        {
+            double inputNumber = double.Parse(Console.ReadLine());
+            printGrade(inputNumber);
+
+        }
+    }
+}
