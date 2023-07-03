68c0e8e0b1a4281ccaea242ccd29905a2889bd74
denisthemenace42 <denismehmed42@gmail.com>
2023-07-03 20:06:46 +0300

    New Lab tasks

---
+++ Csharp-Fundamentals/AssociativeArrays/Labs/P03.WordSynonyms/Program.cs
@@ -0,0 +1,28 @@
+﻿var words = new Dictionary<string,List<string>>();
+
+int lines = int.Parse(Console.ReadLine()) * 2;
+
+
+for (int i = 0; i<lines; i++){
+
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+     if (word != null && synonym != null)
+            {
+                if (!words.ContainsKey(word))
+                {
+                    words[word] = new List<string>();
+                }
+
+                words[word].Add(synonym);
+            }
+}
+
+foreach(var pair in words){
+
+    string word = pair.Key;
+    List<string> synonymList = pair.Value;
+
+    Console.WriteLine($"{word} - {string.Join(", ", synonymList)}");
+}
---
+++ Csharp-Fundamentals/AssociativeArrays/Labs/P04.WordFilter/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+
+foreach(var word in words){
+
+    Console.WriteLine(word);
+}
